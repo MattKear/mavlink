172e4f64579954cabbace653e4e8baa83774f8b3
MattKear <matthewnkear@gmail.com>
2025-09-23 16:02:39 +0100

    update test msg with new message name

--- msg_test.py
+++ msg_test.py
@@ -1,17 +1,15 @@
 # Very Simple script to test that specific messages are available in the pymavlink install
 from pymavlink.dialects.v20 import ardupilotmega as d
 
-NAME = "MAV_CMD_NAV_WAYPOINT_ARC"
-# NAME = "MAV_CMD_NAV_WAYPOINT"
+NAME = "MAV_CMD_NAV_ARC_WAYPOINT"
 
 print(NAME, "exists?:", hasattr(d, NAME), "; value:", getattr(d, NAME, None))
 
-
-cmd_id = d.MAV_CMD_NAV_WAYPOINT_ARC
+cmd_id = d.MAV_CMD_NAV_ARC_WAYPOINT
 e = d.enums['MAV_CMD']          # enum table generated from the XML
 entry = e[cmd_id]               # lookup by numeric id
 
-print(entry.name)               # e.g. 'MAV_CMD_NAV_WAYPOINT_ARC'
+print(entry.name)               # e.g. 'MAV_CMD_NAV_ARC_WAYPOINT'
 print(getattr(entry, 'description', ''))
 
 
